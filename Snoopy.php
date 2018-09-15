f9782dbd9d01264e5596e4a7c2613276982027e2
Bedder <bedderchavez@gmail.com>
2018-09-15 01:16:09 -0300

    Class that simulates a web browser [PHP5]
    Snoopy is a PHP class that simulates a web browser. It automates the task of retrieving web page content and posting forms

--- Snoopy.php
+++ Snoopy.php
@@ -6,22 +6,7 @@
  * Author: Monte Ohrt <monte@ohrt.com>
  * Copyright (c): 1999-2014, all rights reserved
  * Version: 2.0.0
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- *
- * You may contact the author of Snoopy by e-mail at:
- * monte@ohrt.com
+ * License: GNU Lesser General Public License v2.1
  *
  * The latest version of Snoopy can be obtained from:
  * http://snoopy.sourceforge.net/
